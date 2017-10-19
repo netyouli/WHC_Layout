268b4dd98f7e02d2e7950693eec9035ac999d485
netyouli <712641411@qq.com>
2017-10-19 17:02:10 +0800

    更新demo

--- WHC_Layout/AppDelegate.swift
+++ WHC_Layout/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        UITableView.whc_InitConfig()
+        UITableView.whc_InitConfig() // 替代废弃的initialize方法
         // Override point for customization after application launch.
         let rootnv = UINavigationController(rootViewController: ViewController())
         window = UIWindow(frame: UIScreen.main.bounds)
